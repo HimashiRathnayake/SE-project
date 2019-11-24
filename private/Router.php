25c9d2e3495140304c80a5a3312cabcd945b39a5
yasiruRathnayaka97 <57071700+yasiruRathnayaka97@users.noreply.github.com>
2019-11-25 03:37:21 +0530

    Update Router.php

--- private/Router.php
+++ private/Router.php
@@ -16,13 +16,13 @@ class Router{
     public function respond($uri){
          $cmp=Self::uriProcess($uri);
          $cmp=array_chunk($cmp,3);
-         $this->controller=$cmp[1][0];
-         if (count($cmp[1])>1){
-            $this->method=$cmp[1][1];
+         $this->controller=$cmp[0][0];
+         if (count($cmp[0])>1){
+            $this->method=$cmp[0][1];
          }
          $this->parameters=[];
-         if (count($cmp[1])>2){
-            $this->parameters=array_chunk($cmp[1],2)[1];
+         if (count($cmp[0])>2){
+            $this->parameters=array_chunk($cmp[0],2)[1];
         }
         call_user_func_array([$this->controller.'Controller', $this->method],$this->parameters);
     }
