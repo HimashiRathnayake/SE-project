f788026efac9aeda09c95a6e0ba7b7d79c31db23
yasiruRathnayaka97 <57071700+yasiruRathnayaka97@users.noreply.github.com>
2019-11-25 03:33:55 +0530

    Update init.php

--- private/init.php
+++ private/init.php
@@ -61,7 +61,7 @@ $Router=new Router();
 
 $Router->respond($uri);
 $title=$Router->getTitle();   
-echo $uri;
+echo $title;
 ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="../3rdParty/css3/contactForm.css">
